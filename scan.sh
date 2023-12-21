1c0a4c6587aa7c8abc0243b9792df7646aef3b70
Wireless Dan <wirelessdan@proton.me>
2023-12-21 17:48:32 -0500

    ready for test deployment on VPS

--- scan.sh
+++ scan.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 cat banner.txt;
-subfinder -dL targets.txt -o subdomains.txt;
+
+#delete contents of subscan and newdomains
+rm subscan.txt newsubdomains.txt;
+
+#subfinder finds subs, outputs results to file
+subfinder -dL targets.txt -o subscan.txt;
+
+#appends subscan to alldomains, sdout goes to newdowmains.txt
+#run this line ONLY when adding new domains if you don't want a million notifications
+cat subscan.txt | anew allsubdomains.txt >> newsubdomains.txt; 
+
 echo "SUBDOMAINS ENUMERATED, RUNNING NUCLEI";
-nuclei -list subdomains.txt -o nuclei_results.txt;
+
+#nuclei scans subdomains for CVE's
+nuclei -list newsubdomains.txt -o nuclei_results.txt;
+
 echo "NUCLEI SCAN FINISHED, CHECKING FOR NEW RESULTS"
